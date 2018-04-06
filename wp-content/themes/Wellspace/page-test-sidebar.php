a2715ab30d5239bebf3bc0d95e62ca3998769aea
Joan Madsen <joanmadsen@MacBook-Pro.domain>
2018-04-06 12:10:47 -0600

    april 5th

--- wp-content/themes/Wellspace/page-test-sidebar.php
+++ wp-content/themes/Wellspace/page-test-sidebar.php
@@ -62,7 +62,7 @@ $items = wp_get_nav_menu_items('primary', [
 </header>
 <div class="container">
     <div class="row">
-        <div class="col content">
+        <div class="col-sm-8 content">
 
 
 Lorem ipsum dolor amet mumblecore vape gentrify vaporware hot chicken. Kitsch YOLO jean shorts, narwhal snackwave organic master cleanse iPhone disrupt farm-to-table 90's messenger bag next level food truck bespoke. Cardigan pickled vape franzen health goth. Wolf pinterest master cleanse put a bird on it tumblr yuccie semiotics taiyaki messenger bag. Polaroid celiac cronut sartorial vape, typewriter messenger bag forage butcher yuccie enamel pin hammock flexitarian tumeric. Mlkshk copper mug locavore banh mi keytar. Pickled meggings aesthetic put a bird on it tacos.
@@ -75,7 +75,35 @@ I bet you’re still using Bootstrap too…
 
 
         </div>
-
+        <div class="col-sm-4 sidebar">
+            <div class="profile">
+                <h2>Testy McGee, PHD</h2>
+                <h3>Wellspace Utah</h3>
+                <div class="photo"></div>
+                <address>
+                    Wellspace Utah<br/>
+                    123 Test st.<br/>
+                    Salt Lake City, UT 84115
+                </address>
+                <nav class="contact">
+                    <a href="tel:1234567890">1234567890</a>
+                    <a href="mailto:testy@mcgee.com">testy@mcgee.com</a>
+                </nav>
+                <div class="psychology-today"></div>
+            </div>
+            <h2>Contact Us</h2>
+        <form class="contact" action="contact.php" method="post">
+            <label for="first-name">First Name</label>
+            <input type="text" id="first-name" name="first_name" required/>
+            <label for="last-name">Last Name</label>
+            <input type="text" id="last-name" name="last_name" required/>
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" required/>
+            <label for="message">Message</label>
+            <textarea id="message" name="message" required></textarea>
+            <input type="submit" value="Send"/>
+        </form>
+        </div>
     </div>
 </div>
 <footer>
